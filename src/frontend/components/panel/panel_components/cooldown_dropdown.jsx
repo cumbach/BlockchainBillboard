60d6a9abc52e8cac86739d9668e6405f2012b90e
Chris Umbach <chrisumbach@gmail.com>
2018-02-07 10:52:01 -0800

    cooldown strong

--- src/frontend/components/panel/panel_components/cooldown_dropdown.jsx
+++ src/frontend/components/panel/panel_components/cooldown_dropdown.jsx
@@ -39,7 +39,7 @@ class CooldownDropdown extends React.Component {
   render() {
     return (
       <div className='cooldown-dropdown'>
-        Cooldown Time
+        <strong>Cooldown Time</strong>
         {this.displayCooldown()}
       </div>
     );
